ef1ec7329546f5420b8552a9213e469d6a1d026d
AkumuOmolo <akumu.omoloh@gmail.com>
2025-06-14 09:20:00 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+document.addEventListener("DOMContentLoaded" , function() {
+    const nameElement=document.getElementById("text");
+    nameElement.textContent = "This is really cool!";
+
+}) 
 // Your code goes here
